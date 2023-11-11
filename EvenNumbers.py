114e2252b1e19cc025daba32892b970473b87a34
Unknown :) <147919329+simpleprogramer01@users.noreply.github.com>
2023-11-11 18:39:13 +0330

    Add files via upload

---
+++ EvenNumbers.py
@@ -0,0 +1,10 @@
+#simple programer
+#A script to display even numbers in list
+#list :
+even_number = []
+#conditions : 
+for num in range(2, 201, 2):
+    even_number.append(num)
+#prints : 
+print("Even numbers less than 200 : ")
+print(even_number)
